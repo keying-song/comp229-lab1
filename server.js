74dcca472f04754e04fac7370a37f12ede0b3577
Kyeing Song <sk15910766709@gmail.com>
2022-05-28 00:07:50 -0400

    backup

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-const http = require('http');
-const fs = require('fs');
-const port =3000;
-const mime = require('mime-types');
-
-
-let lookup = mime.lookup;
-const server=http.createServer(function(req,res){
-
-  let path = req.url;
-  if(path =="/" || path =="/home")
-  {
-     path = "/index.html";
-
-  }
-  let mime_type = lookup(path.substring(1));
-  console.log(`Mime Type: ${mime_type}`);
-   fs.readFile(__dirname+path, function(err, data)
-   {
-      if(err)
-      {
-         res.writeHead(404);
-         console.log(`Error:${err.message} 404 `);
-        return res.end(err.message);
-      }
-      res.setHeader("X-Content-Type-Options", "nosniff");
-      res.writeHead(200, {'Content-Type': mime_type});
-      //console.log(`Data:${data}`);
-      return res.end(data);
-      
-      
-   });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const port = 3000;
+const mime_types_1 = __importDefault(require("mime-types"));
+let lookup = mime_types_1.default.lookup;
+const server = http_1.default.createServer(function (req, res) {
+    let path = req.url;
+    if (path == "/" || path == "/home") {
+        path = "/index.html";
+    }
+    let mime_type = lookup(path.substring(1));
+    console.log(`Mime Type: ${mime_type}`);
+    fs_1.default.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            console.log(`Error:${err.message} 404 `);
+            return res.end(err.message);
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200, { 'Content-Type': mime_type });
+        return res.end(data);
+    });
 });
-
-server.listen(port, function()
-{
-   console.log(`server running at ${port}`);
+server.listen(port, function () {
+    console.log(`server running at ${port}`);
 });
+//# sourceMappingURL=server.js.map
