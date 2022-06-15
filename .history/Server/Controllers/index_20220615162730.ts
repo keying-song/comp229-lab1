97a1e9a416a9d961189616f34547cc459620e046
Kyeing Song <sk15910766709@gmail.com>
2022-06-15 16:46:10 -0400

    modify login nav bar

---
+++ .history/Server/Controllers/index_20220615162730.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+export function DisplayHomePage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    res.render('index', { title: 'Home', page:'home', displayName: '' });
+}
+
+export function DisplayAboutPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    res.render('index', { title: 'About Me', page:'about', displayName: '' });
+}
+
+export function DisplayProjectsPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    res.render('index', { title: 'My Projects', page:'projects',  });
+}
+
+export function DisplayServicesPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    res.render('index', { title: 'My Services', page:'services' });
+}
+
+export function DisplayContactPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    res.render('index', { title: 'Contact Me', page:'contact' });
+}
+
+
