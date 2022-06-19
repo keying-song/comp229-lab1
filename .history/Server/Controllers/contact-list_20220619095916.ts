7481e76bbe249794cc491090749ceb9e41326a9f
Kyeing Song <sk15910766709@gmail.com>
2022-06-19 16:05:36 -0400

    update edit function

---
+++ .history/Server/Controllers/contact-list_20220619095916.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import Contact from '../Models/contact';
+
+import { UserDisplayName } from '../Util';
+export function DisplayContactList(req: express.Request, res: express.Response, next: express.NextFunction):void
+{
+   Contact.find(function(err, contactsCollection){
+      if(err){
+        console.error(err);
+        res.end(err);
+      }
+      res.render('index',{title: 'Contact List', page: 'contact-list', contacts:contactsCollection, displayName: UserDisplayName(req)});
+
+   }).sort({Name: 1});
+}
+//display add contact page
+export function DisplayAddPage(req: express.Request, res: express.Response, next: express.NextFunction):void
+{
+  
+    return res.render('index', {title: 'Add', page: 'edit', contact:'', displayName: UserDisplayName(req)});
+}
+export function DisplayEditPage(req: express.Request, res: express.Response, next: express.NextFunction): void
+{
+
+
+}
+
+
+// process add contact
+export function ProcessAddPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+   // instantiate a new contact object
+   let newContact = new Contact
+   ({
+       Name: req.body.contactname,
+       Phone: req.body.phone,
+       Email: req.body.email
+   });
+//add the new contact to collection
+   Contact.create(newContact, function(err)
+   {
+       if(err)
+       {
+         
+             console.error(err);
+             res.end(err);     
+       }
+       return res.redirect('/contact-list');
+
+   });
+
+}
+
+export function ProcessEditPage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+
+
+}
+
+export function ProcessDeletePage(req: express.Request, res: express.Response, next: express.NextFunction): void 
+{
+
+}
--- Server/Controllers/contact-list.js
+++ Server/Controllers/contact-list.js
@@ -17,10 +17,18 @@ function DisplayContactList(req, res, next) {
 }
 exports.DisplayContactList = DisplayContactList;
 function DisplayAddPage(req, res, next) {
-    return res.render('index', { title: 'Add', page: 'edit', messages: req.flash('updateMessage'), displayName: (0, Util_1.UserDisplayName)(req) });
+    return res.render('index', { title: 'Add', page: 'edit', contact: '', displayName: (0, Util_1.UserDisplayName)(req) });
 }
 exports.DisplayAddPage = DisplayAddPage;
 function DisplayEditPage(req, res, next) {
+    let id = req.params.id;
+    contact_1.default.findById(id, {}, {}, function (err, contactToEdit) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.render('index', { title: 'Edit', page: 'edit', contact: contactToEdit, displayName: (0, Util_1.UserDisplayName)(req) });
+    });
 }
 exports.DisplayEditPage = DisplayEditPage;
 function ProcessAddPage(req, res, next) {
@@ -39,9 +47,31 @@ function ProcessAddPage(req, res, next) {
 }
 exports.ProcessAddPage = ProcessAddPage;
 function ProcessEditPage(req, res, next) {
+    let id = req.params.id;
+    let updatedContact = new contact_1.default({
+        _id: id,
+        Name: req.body.contactname,
+        Phone: req.body.phone,
+        Email: req.body.email
+    });
+    contact_1.default.updateOne({ _id: id }, updatedContact, function (err) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/contact-list');
+    });
 }
 exports.ProcessEditPage = ProcessEditPage;
 function ProcessDeletePage(req, res, next) {
+    let id = req.params.id;
+    contact_1.default.remove({ _id: id }, function (err) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/contact-list');
+    });
 }
 exports.ProcessDeletePage = ProcessDeletePage;
 //# sourceMappingURL=contact-list.js.map
