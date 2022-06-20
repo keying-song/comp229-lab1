2b77ae11366098686f1c8c070a8c983de55d8110
Kyeing Song <sk15910766709@gmail.com>
2022-06-19 20:55:56 -0400

    add delete conirmation function

---
+++ .history/Client/Scripts/app_20220619161949.js
@@ -0,0 +1,20 @@
+/*app.js-Keying Song 301166693 June 3,2022  */
+
+"use strict";
+(function () {
+    function Start() {
+        console.log("app started!");
+        let XHR = new XMLHttpRequest();
+        XHR.open("GET", "../data/data.json");
+        XHR.send();
+        XHR.addEventListener("readystatechange", function () {
+            if (XHR.readyState == 4 && XHR.status == 200) {
+                console.log("JSON DATE:");
+                console.log("==============");
+                console.log(XHR.responseText);
+            }
+        });
+    }
+    window.addEventListener("load", Start);
+})();
+//# sourceMappingURL=app.js.map
--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -1,15 +1,11 @@
 "use strict";
 (function () {
     function Start() {
-        console.log("app started!");
-        let XHR = new XMLHttpRequest();
-        XHR.open("GET", "../data/data.json");
-        XHR.send();
-        XHR.addEventListener("readystatechange", function () {
-            if (XHR.readyState == 4 && XHR.status == 200) {
-                console.log("JSON DATE:");
-                console.log("==============");
-                console.log(XHR.responseText);
+        console.log("App Started!");
+        $("a.delete").on("click", function (event) {
+            if (!confirm("Are you sure?")) {
+                event.preventDefault();
+                location.href = "/contact-list";
             }
         });
     }
